3202530d4b8075ae2479b7c19bcae010071e8f59
bud_ha <hmh.uhtala11@gmail.com>
2015-02-11 12:24:27 +0200

    shoppincart dropdown proto/data from offer.json

--- public/app.js
+++ public/app.js
@@ -1,15 +1,29 @@
 'use strict'
-angular.module('app', [
-    'ui.router'
-  ])
-  .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
-    $urlRouterProvider.otherwise('/');
 
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'modules/core/views/home.html',
-        controller: 'homeCtrl'
-      })
+var app = angular.module('app', [
+  'ui.router'
+]);
 
-  }]);
+//For configuring providers
+app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'modules/core/views/home.html',
+      controller: 'HomeCtrl'
+    })
+
+  /* app.service('citiesServices', function($http, $q) {
+     var deferred = $q.defer();
+     $http.get('/resources/offers.json').then(function(data) {
+       deferred.resolve(data);
+     });
+     this.getData = function() {
+       return .deferred.promise;
+     }
+
+   });*/
+
+}]);
